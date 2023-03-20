fad7744efa978549e382e3395b57e7ed0e57459d
marcoscutaro <marcostruggia@gmail.com>
2023-03-20 10:14:01 +0000

    swswsws

--- src/models.py
+++ src/models.py
@@ -35,7 +35,23 @@ class Planets (Base):
     # Here we define columns for the table address.
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    name = Column(String(250))
+    name = Column(String(250), nullable=False)
+    rotation_period = Column(String(250))
+    orbital_period = Column(String(250))
+    diameter = Column(String(250))
+    climate = Column(String(250))
+    gravity = Column(String(250))
+    terrain = Column(String(250))
+    surface_water = Column(String(250))
+    population = Column(String(250))
+    favorite_planet = relationship("Favorite")
+
+class Starships (Base):
+    __tablename__ = 'starships'
+    # Here we define columns for the table address.
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
     rotation_period = Column(String(250))
     orbital_period = Column(String(250))
     diameter = Column(String(250))
@@ -51,10 +67,12 @@ class Favorites(Base):
     __tablename__ = 'favorites'
     id = Column(Integer, primary_key=True)
     user_id = Column(Integer, ForeignKey('user.id'))
-    character_favorite = Column(Integer,ForeignKey('character.id'))
-    planet_favorite = Column(Integer,ForeignKey('planet.id'))
+    character_fav = Column(Integer,ForeignKey('character.id'))
     character_relation = relationship ('Character', back_populates = 'favorite_character')
+    planet_fav = Column(Integer,ForeignKey('planet.id'))
     planet_relation = relationship ('Planet', back_populates = 'favorite_planet')
+    starships_fav = Column(Integer,ForeignKey('planet.id'))
+    starships_relation = relationship ('Starships', back_populates = 'favorite_starships')
 
 
 
