2a82b48fb30cb23a534c1f46f808da680c302664
marcoscutaro <marcostruggia@gmail.com>
2023-03-20 10:21:26 +0000

    swsw

--- src/models.py
+++ src/models.py
@@ -14,6 +14,7 @@ class User(Base):
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
+    age = Column(String(250))
 
 class Characters (Base):
     __tablename__ = 'characters'
@@ -52,15 +53,15 @@ class Starships (Base):
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
-    rotation_period = Column(String(250))
-    orbital_period = Column(String(250))
-    diameter = Column(String(250))
-    climate = Column(String(250))
-    gravity = Column(String(250))
-    terrain = Column(String(250))
-    surface_water = Column(String(250))
-    population = Column(String(250))
-    favorite_planet = relationship("Favorite")
+    model = Column(String(250))
+    manufacturer = Column(String(250))
+    cost_in_credits = Column(String(250))
+    max_atmosphering_speed = Column(String(250))
+    crew = Column(String(250))
+    passengers = Column(String(250))
+    cargo_capacity = Column(String(250))
+    consumables = Column(String(250))
+    favorite_starships = relationship("Favorite")
 
 
 class Favorites(Base):
